dc7dbe3fbacf3a4102699eb31a7cf9168e02e5a7
Yuval Bar-Hai <nahemnahem@gmail.com>
2012-12-20 12:36:56 +0200

    added lab7

--- lab6/bcmpAS.asm
+++ lab6/bcmpAS.asm
@@ -1,7 +1,7 @@
 section .text
 	global main
 	global old_main
-	global compare
+	extern compare
 	extern fopen
 	extern fread
 	extern printf
@@ -9,8 +9,8 @@ section .text
 main:
 	push 	ebp
 	mov 	ebp, esp
-	push 	DWORD[ebp + 12]
-	push 	DWORD[ebp + 8]
+	push 	DWORD[ebp + 12] ;argv[0]
+	pus                        h 	DWORD[ebp + 8] ; argc
 	call 	old_main
 	mov 	esp, ebp
 	pop 	ebp
@@ -24,107 +24,29 @@ old_main:
 	sub 	esp, 8 	;local var file1 and file 2*/
 
 	; fopen( file1_name, "r"); */
-	mov 	ebx, [ebp + 12] 	; pointer to argv[0]*/
-	mov 	ebx, [ebx + 4]	; value in argv[1] = "file1.txt"*/
-	push romode ; "r" string
-	push 	ebx ; "file1.txt"
+	mov 	ebx, DWORD[ebp + 12] 	; pointer to argv[0]*/
+	mov 	ebx, DWORD[ebx + 4]	; value in argv[1] = "file1.txt"*/
+	push romode
+	push 	ebx
 	call 	fopen ; pointer to file1 will be in eax */
 	mov 	[ebp -4], eax ; local variable file1 is now FILE *file1 = fopen( file1_name, "r");*/
 	
 	; fopen( file2_name, "r"); */
-	mov 	ebx, [ebp + 12] 	; pointer to argv[0]*/
-	mov 	ebx, [ebx + 8]	; value in argv[2] = "file2.txt"*/
-	push romode ; "r" string
-	push 	ebx ; "file2.txt"
+	mov 	ebx, DWORD[ebp + 12] 	; pointer to argv[0]*/
+	mov 	ebx, DWORD[ebx + 8]	; value in argv[2] = "file2.txt"*/
+	push romode
+	push 	ebx
 	call 	fopen ; pointer to file2 will be in eax */
 	mov 	[ebp -8], eax ; local variable file2 is now FILE *file2 = fopen( file2_name, "r");*/
 
-	push 	DWORD[ebp-8] ; we need file2 for compare (var2 - void compare(FILE *file1, FILE *file2);) 
-	push 	DWORD[ebp-4] ; we need file1 for compare (var1 - void compare(FILE *file1, FILE *file2);)
+	push 	DWORD[ebp-8]
+	push 	DWORD[ebp-4]
 	call 	compare;
 	; 3 last lines in every function*/
 	mov 	esp, ebp
 	pop 	ebp
 	ret
- 
-;fopen:
-	; 2 first lines in every function*/
-;	push	ebp
-;	mov 	ebp, esp
-;	
-;	mov 	eax, 3
-;	mov 	ebx, [ebp+8] ; file1.txt / file2.txt
-;	mov 	ecx, [ebp+12] ; "r"
-;	int 	0x80
- ;	; 3 last lines in every function*/
-;	mov 	esp, ebp
-;	pop 	ebp
-;	ret
-;	
-	
-compare:
-	; 3 first lines in every function*/
-	push 	ebp
-	mov 	ebp, esp
-	sub 	esp, 12 ;local var counter, unsigned int letter1, unsigned int letter2*/   
-	mov 	DWORD[ebp-12], 0; unsigned int letter2 = 0;
-	mov 	DWORD[ebp-8], 0; unsigned int letter1 = 0;
-	mov 	DWORD[ebp-4], 0;  int counter = 0;
-.LOOP:
-	; fread(&letter1, 1, 1, file1)
-	;we need the address of ebp-8, but we can't write "mov ebx, ebp-8" - iligal
-	; we can't write "mov ebx, [ebp-8]" --> value and not address
-	; so we will use lea
-	lea 	ebx, [ebp-8]; &letter1	
-	push 	DWORD [ebp+8] ; file1
-	push 	1	; 1
-	push 	1	; 1
-	push 	ebx ; file1
-	call 	fread; eax will have the return value
-	mov 	ecx, eax; ecx will keep the value
-
-	; fread(&letter2, 1, 1, file2)
-	;we need the address of ebp-12, but we can't write "mov ebx, ebp-12" - iligal
-	; we can't write "mov ebx, [ebp-12]" --> value and not address
-	; so we will use lea
-	lea 	ebx, [ebp-12];&letter2	
-	push 	DWORD [ebp+12] ; file2
-	push 	1	; 1
-	push 	1	; 1
-	push 	ebx ; file1
-	call 	fread; eax will have the return value
-	mov 	edx, eax; edx will keep the value	
-	
-	; stop condition (while(fread(&letter1, 1, 1, file1) & fread(&letter2, 1, 1, file2)) {)
-	test 	cl, cl ; if fread(&letter1, 1, 1, file1) == 0
-	je 	.END_LOOP
-	test 	dl, dl ; if fread(&letter1, 1, 1, file2) == 0
-	je 	.END_LOOP
-	; here is the loop
-	mov 	eax, DWORD [ebp-12] ;letter2
-	mov 	ebx, DWORD [ebp-8] ;letter1
-	cmp 	eax, ebx 
-	je    .AFTER_PRINT; if ( letter1 != letter2 ){
-	;printf("byte %d -%d +%d\n",  counter+1, letter1, letter2 );
-	push	eax ;letter2
-	push	ebx ;letter1
-	;counter+1
-	mov 	ecx, DWORD [ebp-4]
-	add 	ecx , 1
-	push	ecx
-	push	message ; "byte %d -%d +%d\n" 
-	call printf
-	
-.AFTER_PRINT:	
-	add 	DWORD [ebp-4], 1 ;counter ++;
-	jmp .LOOP
-	; end of loop
-.END_LOOP:
- 	; 3 last lines in every function*/
-	mov 	esp, ebp
-	pop 	ebp
-	ret   
+      
 
 section .data
 	romode 	db "r",0
-	message db "byte %d -%d +%d", 0xa
--- lab6/bfix.c
+++ lab6/bfix.c
@@ -5,55 +5,23 @@
 #include "diff.h"
 
 int main(int argc, char **argv) {
-      int print = 0;
-      while (1) {
-	      char c;
-	      c = getopt (argc, argv, "hrm\n");
-	      if (c == -1) {
-		  break;
-	      }
-	      switch (c) {
-	      case 'h':
-		  printf ("OPTIONS\n");
-		  printf ("-h  print a summary of options and exit\n");
-		  printf ("-r  reverse the differences\n");
-		  printf ("-m  print a message each time a change is applied\n");
-		  break;
-	      case 'r':
-		  break;
-	      case 'm':
-		  print = 1;
-		  break;	    
-	      default:
-		  printf ("default\n");
-	      }
-      }  	
-      char *file_diff = argv[argc-1];
-      char *file_to_fix = argv[argc-2];      
+	
+      char *file_diff = argv[2];
+      char *file_to_fix = argv[1];      
       FILE *file_differences = fopen( file_diff, "r");
       FILE *file1 = fopen( file_to_fix, "r+");
       
       struct diff diff;
       int counter = 0;
-      if (!file_differences){
-	printf("Can't open the file_differences\n" );
-	return 0;
-      }
-      if (!file1){
-	printf("Can't open the file1\n" );
-	return 0;
-      }      
+     
       char line[64];
       while( fgets(line,sizeof(line),file_differences) != NULL ){
 	parsediff(line, &diff);
 	fseek( file1, diff.offset-1, SEEK_SET );
 	fwrite( &diff.new, 1, 1, file1 );
-	if ( print ){
-	  printf("A change has been applied\n");
-	}
       }
-      fclose(file_differences);
-      fclose(file1);
-      return 0;
+      
+      
+    return 0;
 }
 
